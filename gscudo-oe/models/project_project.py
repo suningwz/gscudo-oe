0e31dad1dd82b2dcdf75926a9d7ece5788fcb867
luigi.gregori <luigi.gregori@grupposcudo.it>
2021-08-13 09:23:53 +0000

    aggiunti riferimenti ai project di sawgest

--- gscudo-oe/models/project_project.py
+++ gscudo-oe/models/project_project.py
@@ -6,3 +6,16 @@ class ProjectProject(models.Model):
 
     department_id  = fields.Many2one(comodel_name='hr.department', string='Dipartimento')
     
+    sg_offer = fields.Char(string='Rif Offerta')
+    sg_offer_id = fields.Integer(string='ID Offerta SawGest')
+    sg_url = fields.Char(string='Vedi in sawgest' ,compute="_compute_sg_url", store=False )
+
+    def _compute_sg_url(self):
+        irconfigparam = self.env['ir.config_parameter']
+        base_url = irconfigparam.sudo().get_param('sawgest_offers_url')
+        if base_url:
+            for record in self:
+                if record.sg_offer_id and record.sg_offer_id > 0:
+                    record.sg_url = base_url.format(record.sg_offer_id)    
+                else:
+                    record.sg_url = False
