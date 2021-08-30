ce8b877438e3980ae6964b64e75c71ef32f92b6b
luigi.gregori <luigi.gregori@grupposcudo.it>
2021-08-30 09:27:20 +0000

    predisposizione per position e department

--- gscudo-itasset/models/gs_itasset.py
+++ gscudo-itasset/models/gs_itasset.py
@@ -8,11 +8,13 @@ class ItAsset(models.Model):
     _description = 'Assets per la gestione dell''IT e dei dipendenti'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string="Cespite", track_visibility=True)
-    description = fields.Char(string="Descrizione" ,track_visibility=True)
-    active = fields.Boolean(string='Attivo' , default= True ,track_visibility=True)
-    itasset_type_id = fields.Many2one(comodel_name='gs_itasset_type', string='Tipologia',  track_visibility=True)
-    employee_id = fields.Many2one(comodel_name='hr.employee', string='Assegnatario',  track_visibility=True)
+    name = fields.Char(string="Cespite",  tracking=True)
+    description = fields.Char(string="Descrizione" , tracking=True)
+    active = fields.Boolean(string='Attivo' , default= True , tracking=True)
+    #itasset_position = fields.Char(string="Sede")
+    itasset_type_id = fields.Many2one(comodel_name='gs_itasset_type', string='Tipologia',  tracking=True)
+    employee_id = fields.Many2one(comodel_name='hr.employee', string='Assegnatario',  tracking=True)
+    #department = fields.Char(string='Ufficio', related = 'employee_id.department_id.name')
     note = fields.Text(string='Annotazioni')
     
     
