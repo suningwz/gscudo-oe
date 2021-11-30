e99e34ac472e9706c71383f63a0ed8c3f4fb976a
luigi.gregori <luigi.gregori@grupposcudo.it>
2021-11-30 13:37:21 +0000

    fix nome modello

--- gscudo-oe/models/gs_worker_job_type.py
+++ gscudo-oe/models/gs_worker_job_type.py
@@ -2,13 +2,14 @@ from odoo import api, fields, models
 
 
 class WorkerJob(models.Model):
-    _name = 'gs_worker_job'
+    _name = 'gs_worker_job_type'
     _description = 'Mansione'
 
     name = fields.Char(string='Name')
+    active = fields.Boolean(string='Attivo', default=True)
     gs_worker_contract_id = fields.Many2one(comodel_name='gs_worker_contract_id', string='Contratto')
     gs_worker_job_type = fields.Many2one(comodel_name='gs_worker_job_type', string='Mansione Standard')
     cartsan_id  = fields.Integer(string='ID CartSan')
-    
+
     
     
