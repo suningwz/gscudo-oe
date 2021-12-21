7b36f8ce8116f36198c9408f3a7b74f4af57da43
Davide Bazzan <davide.bazzan@grupposcudo.it>
2021-12-21 18:01:17 +0000

    Aggiunto tag manifest 1.1

--- gscudo-oe/__manifest__.py
+++ gscudo-oe/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.9', 
+    'version': '1.1', 
 
     # any module necessary for this one to work correctly
     'depends': ['base',
