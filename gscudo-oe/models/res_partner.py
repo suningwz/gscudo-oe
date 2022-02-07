555c64458467afae7b83500ddb373278b02807b4
Luigi Gregori <lg@lgit.it>
2022-02-07 17:06:47 +0000

    aggiunta gestione cliente su Lead e Partner

--- gscudo-oe/models/res_partner.py
+++ gscudo-oe/models/res_partner.py
@@ -81,48 +81,51 @@ class ResPartner (models.Model):
     is_backoffice = fields.Boolean(string="E' Backoffice", default=False)
     
 
-   ##### Competitors
+    ##### Competitors
     def get_competitor_type(self):
         return [('','non definito '),
                 ('int','interno '),
-                ('est','esterno ')
+                ('est','esterno '),
+                ('cli','cliente')
                 ]
         
 
-    safety_competitor_type  = fields.Selection(get_competitor_type, string='Sicurezza gestione',  default='')
+    safety_competitor_type  = fields.Selection(get_competitor_type, string='Sicurezza gestione',  default='', tracking=True)
     safety_partner_id = fields.Many2one(string='Conc. Sicurezza', comodel_name='res.partner',
-                                        domain="[('is_competitor','=',True)]")
+                                        domain="[('is_competitor','=',True)]", tracking=True)
 
-    training_competitor_type  = fields.Selection(get_competitor_type, string='Formazione gestione', default='')
+    training_competitor_type  = fields.Selection(get_competitor_type, string='Formazione gestione', default='', tracking=True)
     training_partner_id = fields.Many2one(string='Conc. Formazione', comodel_name='res.partner',
-                                        domain="[('is_competitor','=',True)]")
+                                        domain="[('is_competitor','=',True)]", tracking=True)
     
-    food_competitor_type  = fields.Selection(get_competitor_type, string='Alimentare gestione', default='')
+    food_competitor_type  = fields.Selection(get_competitor_type, string='Alimentare gestione', default='', tracking=True)
     food_partner_id = fields.Many2one(string='Conc. Alimentare', comodel_name='res.partner',
-                                        domain="[('is_competitor','=',True)]")
+                                        domain="[('is_competitor','=',True)]", tracking=True)
 
-    machdir_competitor_type  = fields.Selection(get_competitor_type, string='Dirett. Macchine gestione', default='')
+    machdir_competitor_type  = fields.Selection(get_competitor_type, string='Dirett. Macchine gestione', default='', tracking=True)
     machdir_partner_id = fields.Many2one(string='Conc. Dirett. Macchine', comodel_name='res.partner',
-                                        domain="[('is_competitor','=',True)]")
+                                        domain="[('is_competitor','=',True)]", tracking=True)
 
-    healthsurv_competitor_type  = fields.Selection(get_competitor_type, string='Sorv. Sanit. gestione', default='')
+    healthsurv_competitor_type  = fields.Selection(get_competitor_type, string='Sorv. Sanit. gestione', default='', tracking=True)
     healthsurv_partner_id = fields.Many2one(string='Conc. Sorv. Sanit.', comodel_name='res.partner',
-                                        domain="[('is_competitor','=',True)]")
+                                        domain="[('is_competitor','=',True)]", tracking=True)
     
-    environment_competitor_type  = fields.Selection(get_competitor_type, string='Ambientale gestione', default='')
+    environment_competitor_type  = fields.Selection(get_competitor_type, string='Ambientale gestione', default='', tracking=True)
     environment_partner_id = fields.Many2one(string='Conc. Ambientale', comodel_name='res.partner',
-                                        domain="[('is_competitor','=',True)]")
+                                        domain="[('is_competitor','=',True)]", tracking=True)
 
-    management_competitor_type  = fields.Selection(get_competitor_type, string='Sistemi Gest. gestione', default='')
+    management_competitor_type  = fields.Selection(get_competitor_type, string='Sistemi Gest. gestione', default='', tracking=True)
     management_partner_id = fields.Many2one(string='Conc. Sistemi Gest.', comodel_name='res.partner',
-                                        domain="[('is_competitor','=',True)]")
+                                        domain="[('is_competitor','=',True)]", tracking=True)
 
     has_competitors  = fields.Boolean(string='Ci sono concorrenti', compute='_compute_has_competitor', store=True)
-
+    is_customer  = fields.Boolean(string='E\' cliente', compute='_compute_has_competitor', store=True)
+    
     @api.depends('safety_competitor_type', 'training_competitor_type', 'food_competitor_type', 'machdir_competitor_type', 'healthsurv_competitor_type', 'environment_competitor_type')                                  
     def _compute_has_competitor(self):
         for record in self:
             record.has_competitors = False
+            record.is_customer = False
                 
             if  (record.safety_competitor_type == "est" or 
                         record.training_competitor_type == "est" or 
@@ -131,7 +134,15 @@ class ResPartner (models.Model):
                         record.healthsurv_competitor_type == "est" or 
                         record.environment_competitor_type == "est") :
                 record.has_competitors = True
-                
+            
+            if  (record.safety_competitor_type == "cli" or 
+                        record.training_competitor_type == "cli" or 
+                        record.food_competitor_type == "cli" or 
+                        record.machdir_competitor_type == "cli" or 
+                        record.healthsurv_competitor_type == "cli" or 
+                        record.environment_competitor_type == "cli") :
+                record.is_customer = True
+   
 
 
     
