2c6fee21d53a37d24323d2fc37b2c123b39f45f4
Luigi Gregori <lg@lgit.it>
2022-03-22 08:46:57 +0000

    filtro linguetta su gruppo surveillance

--- gscudo-surveillance/models/gs_worker_medical_check.py
+++ gscudo-surveillance/models/gs_worker_medical_check.py
@@ -1,11 +1,14 @@
 from odoo import api, fields, models
 
+import datetime
+from datetime import timezone, datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
 
 class WorkerMedicalCheck(models.Model):
     _name = 'gs_worker_medical_check'
     _description = 'Esami / Visite'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', readonly=True)
     active = fields.Boolean(string='Attivo', default = True)
     gs_worker_id = fields.Many2one(comodel_name='gs_worker', string='Lavoratore')
     
@@ -15,6 +18,47 @@ class WorkerMedicalCheck(models.Model):
     schedule_time = fields.Datetime(string='Data ora prenotazione')
     gs_medical_check_frequency_id = fields.Many2one(comodel_name='gs_medical_check_frequency', string='Frequenza')
     note = fields.Text(string='Note')
+
+    expired = fields.Boolean(string='Scaduto', compute='_compute_expiration', )
+    expiring = fields.Boolean(string='In scadenza', compute='_compute_expiration', )
+ 
+    @api.onchange('gs_medical_check_type_id')
+    def _onchange_checktype_id(self):
+        for record in self:
+            record.gs_medical_check_frequency_id= record.gs_medical_check_type_id.gs_medical_check_frequency_id
+
+    @api.onchange('execution_date','gs_medical_check_frequency_id')
+    def _compute_expiry_date(self):
+        for record in self:
+            if record.execution_date != False and record.gs_medical_check_frequency_id != False:
+                record.expiry_date = record.execution_date + relativedelta(
+                                years=record.gs_medical_check_frequency_id.year_interval,
+                                months=record.gs_medical_check_frequency_id.month_interval,
+                                days=record.gs_medical_check_frequency_id.day_interval)
+
+    @api.onchange('gs_worker_id','gs_medical_check_type_id', 'execution_date')
+    def _compute_name(self):
+        for record in self:
+            record.name = "{} {} {} ".format(
+                    record.gs_worker_id.name or "", 
+                    record.gs_medical_check_type_id.name or "", 
+                    record.execution_date.strftime("%Y-%m-%d"))
+
+    @api.depends('expiry_date')
+    def _compute_expiration(self):
+        for record in self:
+            record.expired=False
+            record.expiring = False
+            if record.expiry_date != False:
+                if record.expiry_date < datetime.now().date():
+                    record.expired=True
+                    record.expiring = False
+                elif record.expiry_date < datetime.now().date()+ relativedelta(months=2):
+                    record.expired=False
+                    record.expiring = True
+
+
+
 
 
 class GSWorker(models.Model):
