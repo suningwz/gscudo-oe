f4fbc56bdfe0d8b67ba8c2bc44e236812397713f
Luigi Gregori <lg@lgit.it>
2022-03-22 08:46:26 +0000

    aggiornamento manifest a v 1.8

--- gscudo-oe/__manifest__.py
+++ gscudo-oe/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'GruppoScudo',
-    'version': '1.7', 
+    'version': '1.8', 
 
     # any module necessary for this one to work correctly
     'depends': ['base',
