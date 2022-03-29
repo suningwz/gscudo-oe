2f279d460e4db0d547d23c7e5e46197cbcd2c5e3
Luigi Gregori <lg@lgit.it>
2022-03-29 17:38:27 +0000

    Aggiunta dipendenza da contacts

--- gscudo-oe/__manifest__.py
+++ gscudo-oe/__manifest__.py
@@ -22,7 +22,7 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base',
-                # 'contacts',
+                'contacts',
                 'project',
                 'hr_timesheet',
                 # 'helpdesk',
