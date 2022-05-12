e869b4f76a9fd3d2b89e418e8db13a83338be8dd
David Barozzini <david.barozzini@grupposcudo.it>
2022-05-12 07:15:49 +0000

    started refactoring

--- gscudo-itasset/models/gs_credential.py
+++ gscudo-itasset/models/gs_credential.py
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class Credential(models.Model):
-    _name = 'gs_credential'
-    _description = 'Credenziali di prima assegnazione'
-    _inherit = ['mail.thread',
-                'mail.activity.mixin']
+    _name = "gs_credential"
+    _description = "Credenziali di prima assegnazione"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
 
-    name = fields.Char(string="Username",  tracking=True)
+    name = fields.Char(string="Username", tracking=True)
     temp_pwd = fields.Char(string="Password", tracking=True)
-    credential_env = fields.Selection(string='Ambiente',
-                                   selection=[('Dominio', 'Dominio'),
-                                              ('Email', 'Email'),
-                                              ('SawGest', 'SawGest'),
-                                              ('Esolver', 'Esolver'), 
-                                              ('Odoo', 'Odoo'),])
+    credential_env = fields.Selection(
+        string="Ambiente",
+        selection=[
+            ("Dominio", "Dominio"),
+            ("Email", "Email"),
+            ("SawGest", "SawGest"),
+            ("Esolver", "Esolver"),
+            ("Odoo", "Odoo"),
+        ],
+    )
 
     employee_id = fields.Many2one(
-        comodel_name='hr.employee', string='Assegnatario',  tracking=True)
-
-    
+        comodel_name="hr.employee", string="Assegnatario", tracking=True
+    )
--- gscudo-itasset/models/gs_itasset_type.py
+++ gscudo-itasset/models/gs_itasset_type.py
@@ -1,11 +1,11 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ITAssetType(models.Model):
-    _name = 'gs_itasset_type'
-    _description = 'Tipo di asset'
+    _name = "gs_itasset_type"
+    _description = "Tipo di asset"
 
-    name = fields.Char(string='Tipo Asset')
-    family = fields.Char(string='Famiglia')
-    
-    active = fields.Boolean(string='Attivo' , default= True)    
+    name = fields.Char(string="Tipo asset")
+    family = fields.Char(string="Famiglia")
+
+    active = fields.Boolean(string="Attivo", default=True)
