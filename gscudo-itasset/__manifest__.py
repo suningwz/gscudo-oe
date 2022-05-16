d6730f58a593cc298157b3cda72feac91faa765e
Luigi Gregori <lg@lgit.it>
2022-05-16 15:09:41 +0000

    aggiornamento numero versione

--- gscudo-itasset/__manifest__.py
+++ gscudo-itasset/__manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "GruppoScudo",
-    "version": "14.0.1.10",
+    "version": "14.0.1.11",
     # any module necessary for this one to work correctly
     "depends": ["base", "web", "hr"],
     # always loaded
