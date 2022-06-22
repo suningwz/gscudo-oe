31427c468df0578d931b189b04d688f672eb5b11
David Barozzini <david.barozzini@grupposcudo.it>
2022-06-22 08:39:56 +0000

    version 14.0.1.16

--- gscudo-training/__manifest__.py
+++ gscudo-training/__manifest__.py
@@ -10,7 +10,7 @@
     "author": "Gruppo Scudo Srl / LGIT",
     "website": "http://www.grupposcudo.it",
     "category": "GruppoScudo",
-    "version": "14.0.1.15a",
+    "version": "14.0.1.16",
     "depends": ["base", "sale", "gscudo-oe", "documents"],
     "data": [
         "security/security_groups.xml",
