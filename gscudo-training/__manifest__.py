99ad9f9905bba47c65eaad9c069d04c39669abe8
David Barozzini <david.barozzini@grupposcudo.it>
2022-07-27 11:16:19 +0000

    training v14.0.1.20b

--- gscudo-training/__manifest__.py
+++ gscudo-training/__manifest__.py
@@ -10,7 +10,7 @@
     "author": "Gruppo Scudo Srl / LGIT",
     "website": "http://www.grupposcudo.it",
     "category": "GruppoScudo",
-    "version": "14.0.1.20a",
+    "version": "14.0.1.20b",
     "depends": ["base", "sale", "gscudo-oe", "documents", "gscudo-worddoc"],
     #"external_dependencies": {"python": ["docxtpl"]},
     "data": [
