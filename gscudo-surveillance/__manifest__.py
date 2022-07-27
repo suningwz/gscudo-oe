5d42708e1fe99c3f6919f227362cf82097877724
David Barozzini <david.barozzini@grupposcudo.it>
2022-07-27 11:24:06 +0000

    surveillance v14.0.1.20a

--- gscudo-surveillance/__manifest__.py
+++ gscudo-surveillance/__manifest__.py
@@ -21,7 +21,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'GruppoScudo',
-    "version": "14.0.1.20",
+    "version": "14.0.1.20a",
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'gscudo-oe'],
