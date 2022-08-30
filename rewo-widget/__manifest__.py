526ed90cf30ed99166f287b0c34e271081d05ed9
David Barozzini <david.barozzini@grupposcudo.it>
2022-08-30 10:55:46 +0000

    version 14.0.1.21

--- rewo-widget/__manifest__.py
+++ rewo-widget/__manifest__.py
@@ -11,7 +11,7 @@
     "author": "Gruppo Scudo Srl / LGIT",
     "website": "http://www.grupposcudo.it",
     "category": "GruppoScudo",
-    "version": "14.0.1.20h",
+    "version": "14.0.1.21",
     "depends": [
         "base",
         "contacts",
