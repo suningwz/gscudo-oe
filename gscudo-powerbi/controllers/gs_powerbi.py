93fbffd519ba56e0f9d10424ce9eaa448dd6a66f
Luigi Gregori <lg@lgit.it>
2022-09-07 16:03:31 +0000

    improve to use basic authentication

--- gscudo-powerbi/controllers/gs_powerbi.py
+++ gscudo-powerbi/controllers/gs_powerbi.py
@@ -3,29 +3,31 @@ from odoo import http
 from odoo.http import request
 from werkzeug.wrappers import Request, Response
 import json
-
+import base64
 
 class GsPowerBI(http.Controller):
 
     def _authenticate_user(self):
-        if "HTTP_CLIENT_KEY" in request.httprequest.environ  :
-            client_key =request.httprequest.environ["HTTP_CLIENT_KEY"]
-       
-        if "client_key" in request.params:
-            client_key = request.params["client_key"]   
-       
+
+        if "HTTP_CLIENT_KEY" in request.httprequest.environ:
+            client_key = request.httprequest.environ["HTTP_CLIENT_KEY"]
+        elif "HTTP_AUTHORIZATION" in request.httprequest.environ:
+            client_key = request.httprequest.environ["HTTP_AUTHORIZATION"]
+            if client_key.startswith("Basic "):
+                client_key = base64.b64decode(client_key[6:]).decode()
+            else:
+                client_key = None
+        elif "client_key" in request.params:
+            client_key = request.params["client_key"]
+        else:
+            client_key = None
+
         if client_key:
             username,password=client_key.split(":")
         
             user_id = request.env["res.users"].sudo().search([("login", "=", username)])
             
             if user_id and request.env['res.users.apikeys']._check_credentials(scope='rpc', key=password) == user_id.id:
-              
-                # user_id._check_credentials(password,{"interactive":False})
-                # http.request.session.uid = user_id.id
-                # http.request.session.login = user_id.login
-                # http.request.session.password = user_id.password
-                # http.request.session.get_context()
                 return user_id
 
         raise Exception("Invalid Credentials")
@@ -48,31 +50,18 @@ class GsPowerBI(http.Controller):
         else:
             return Response(json.dumps({"error":"Query not found"}),status=404)
 
-    @http.route("/gscudo-powerbi/get_data", type="http", auth="public")
+    @http.route("/gscudo-powerbi/test", type="http", auth="public")
     def get_data(self, **kw):
 
-        client_key =request.httprequest.environ["HTTP_CLIENT_KEY"] if "HTTP_CLIENT_KEY" in request.httprequest.environ else "admin:fe374a3e95925f80fa49d905612991825288dfe5"
-         
-        if client_key:
-            username,password=client_key.split(":")
+        try:
+            user=self._authenticate_user()
+        except Exception as e:
+            return Response(json.dumps({"error":"Invalid Credentials"}),status=401)
         
-            user_id = request.env["res.users"].sudo().search([("login", "=", username)])
-            
-            if user_id:
-                try:
-                    user_id._check_credentials(password,{"interactive":False})
-                except:
-                    return Response(json.dumps({"error":"Invalid Credentials"}),status=401)
-                http.request.session.uid = user_id.id
-                http.request.session.login = user_id.login
-                http.request.session.password = user_id.password
-                http.request.session.get_context()
-        
-
-        print(request.env.user.has_group("base.group_system"))
-        sql = "SELECT id, login from res_users"
+       
+        sql = "SELECT id, login from res_users where id=%(uid)s"
         if "sql" in request.params:
             sql = request.params["sql"]
-        request.env.cr.execute(sql)
+        request.env.cr.execute(sql,{"uid":user.id })
         res = request.env.cr.dictfetchall()
         return Response(json.dumps(res, default=str), mimetype="application/json")
